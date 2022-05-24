b56fbd1178db0204b4bcf152d949699193a33465
ShinMini <77220824+ShinMini@users.noreply.github.com>
2022-05-24 17:38:15 +0900

    update App.js -> adjust react version 6
    divide navigation bar codes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import * as React from "react";
 import { NavLink, Outlet } from 'react-router-dom';
-import './App.css';
-
-const style = {
-    home: {
-        backgroundColor: 'skyblue',
-    },
-    members: {
-        backgroundColor: 'pink',
-    },
-    projects: {
-        backgroundColor: 'green',
-    },
-};
+import Navigation from './Navigation';
+import './Style/App.css';
 
 export default function App() {
     return (
         <div>
-            <h1>React router Example</h1>
-            <nav style={{ borderBottom: 'solid 1px gray', paddingBottom: '1rem' }}>
-                <NavLink to={'/home'} activeClassName={style.home}>Home</NavLink> | {''}
-                <NavLink to={'/members'} activeClassName={style.members}>members</NavLink> | {''}
-                <NavLink to={'/projects'} activeClassName={style.projects}>projects</NavLink> | {''}
-            </nav>
+            <Navigation />
             <Outlet />
         </div>
     );
