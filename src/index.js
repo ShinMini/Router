4a0441c8c91fa77552f3376a2af3fd0eb8047b5f
ShinMini <77220824+ShinMini@users.noreply.github.com>
2022-05-24 17:39:07 +0900

    update import function names

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Detail from './component/Detail';
-import Details from './component/Details';
+import Members from './component/Members';
+import Projects from './component/Projects';
 import Home from './component/Home';
 
 const rootElement = document.getElementById('app');
@@ -12,8 +12,8 @@ ReactDOM.render(
     <Routes>
       <Route path={'/'} element={<App />}>
         <Route path={'home'} element={<Home />} />
-        <Route path={'memebers'} element={<Detail />} />
-        <Route path={'projects'} element={<Details />} />
+        <Route path={'members'} element={<Members />} />
+        <Route path={'projects'} element={<Projects />} />
       </Route>
     </Routes>
   </BrowserRouter>,
