cc4fa420f72ab6df1e1cf5e8f5e002b07da2aa80
ShinMini <77220824+ShinMini@users.noreply.github.com>
2022-05-24 17:38:48 +0900

    remove useless file -> change memebers, projects file

---
+++ src/component/Members.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function Members() {
+   return (
+      <members>
+         <h1> Members </h1>
+         <p> Jonson </p>
+         <p> James </p>
+         <p> Hyeonmins </p>
+      </members>
+   );
+}
---
+++ src/component/Projects.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default function Projects() {
+   return (
+      <projects>
+         <h1> Projects List</h1>
+         <ul>
+            <li>project 1...</li>
+            <li>project 2...</li>
+            <li>project 3...</li>
+         </ul>
+      </projects>
+   );
+}
